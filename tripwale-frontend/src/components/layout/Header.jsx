e528286d70e990193c36c15d9b449f1fe25479a5
Ankit Patel <ankitacdoctor@gmail.com>
2026-02-07 17:57:41 +0530

    update header about page and images of team

--- tripwale-frontend/src/components/layout/Header.jsx
+++ tripwale-frontend/src/components/layout/Header.jsx
@@ -114,8 +114,8 @@ const Header = () => {
                   src={Tripwalelogo} 
                   alt="TripWale.in"
                   sx={{ 
-                    width: 60, 
-                    height: 60,
+                    width: 100, 
+                    height: 100,
                     borderRadius: 1
                   }}
                 />
